495fb10203a1bf12d55a1001ccef31ecaec58d78
aditimewada <imaditim@gmail.com>
2017-10-06 18:27:05 +0530

    image uploading

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    = require("express");
 var login = require('./routes/loginroutes');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -10,6 +11,30 @@ app.use(function(req, res, next) {
     next();
 });
 var router = express.Router();
+
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Images");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+app.get("/timeline", function (req, res) {
+    res.sendFile(__dirname + "/timeline.html");
+});
+
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!.");
+    });
+});
 // test route
 router.get('/', function(req, res) {
     res.json({
